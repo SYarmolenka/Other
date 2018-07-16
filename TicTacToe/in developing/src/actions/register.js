9623ab168619147bc9e87626cccba34d2b928c57
Sergey Yarmolenka <jibickih@gmail.com>
2018-07-16 15:35:26 +0300

    update data

--- TicTacToe/in developing/src/actions/register.js
+++
@@ -1,7 +0,0 @@
-export const CHANGE_REG = 'CHANGE_REG';
-
-export const change = (name, value) => ({
-  type: CHANGE_REG,
-  name,
-  value
-});
--- TicTacToe/in developing/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import {game} from './game';
-import {register} from './register';
-
-export default combineReducers({
-  game,
-  register
-});
